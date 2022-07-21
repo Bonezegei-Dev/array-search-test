4847ac45dd766580b6e9a123a1855c241d26291a
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2022-07-21 13:40:49 +0800

    Update simple.c

--- sample/simple.c
+++ sample/simple.c
@@ -5,9 +5,23 @@
 char *data="device=100; active=78;";
 
 //varibles
-int
+int device=0;
+int active=0;
 
 int main(){
-  
+   for(int a=0; a<strlen(data); a++){
+        if(src(dat,"device=",a)==0){
+            a+=7;
+            src2(dat,a);
+            device=atoi(src2data);
+            a+=src2cnt;
+         }
+         if(src(dat,"active=",a)==0){
+            a+=7;
+            src2(dat,a);
+            active=atoi(src2data);
+            a+=src2cnt;
+         }
+    }
   
 }
